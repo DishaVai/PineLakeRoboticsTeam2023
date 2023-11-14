96020ac0e0df0f8070c91a17ceab205aad61d299
DishaVai <disha.v@outlook.com>
2023-11-13 16:13:24 -0800

    fixed a bug in autonomous code causing code to loop.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ModularizedBlueFarAutonomous.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ModularizedBlueFarAutonomous.java
@@ -2,19 +2,10 @@ package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.util.ElapsedTime;
-import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-import com.qualcomm.robotcore.hardware.CRServo;
-import org.firstinspires.ftc.vision.tfod.TfodProcessor;
-import org.firstinspires.ftc.vision.VisionPortal;
-import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
-import org.firstinspires.ftc.robotcore.external.tfod.Recognition;
-import com.qualcomm.robotcore.util.ElapsedTime;
 import org.firstinspires.ftc.teamcode.autonomous.Movement;
 
-@Autonomous(name = "blue-far-auto-test")
+//Blue far position parking code using PID controller
+@Autonomous(name = "blue-far-auto")
 public class ModularizedBlueFarAutonomous extends LinearOpMode{
     public void runOpMode() {
         Movement move = new Movement();
@@ -29,11 +20,9 @@ public class ModularizedBlueFarAutonomous extends LinearOpMode{
 
 
         if (opModeIsActive()) {
-            while (opModeIsActive()) {
-                move.moveForwardToThree(-1700, 100);
-                move.resetPower();
-                move.left(-500, 25);
-                //move.strafeLeft(2000,-25);
-            }}
+            move.moveForwardToThree(-1900, 100);
+            move.resetPower();
+            move.strafeLeft(3450, 25);
+        }
     }
 }
