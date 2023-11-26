5e10741b1e0e8f54a8392403c65650c877ba6240
DishaVai <disha.v@outlook.com>
2023-11-26 12:50:01 -0800

    added copies of autonomous code with a five second delay

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BlueCloseDelay.java
@@ -0,0 +1,28 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import org.firstinspires.ftc.teamcode.autonomous.Movement;
+
+@Autonomous(name = "blue-close-delay")
+
+public class BlueCloseDelay extends LinearOpMode{
+    public void runOpMode() {
+        Movement move = new Movement();
+        move.initialize(hardwareMap);
+
+
+        // Wait for the DS start button to be touched.
+        telemetry.addData("DS preview on/off", "3 dots, Camera Stream");
+        telemetry.addData(">", "Touch Play to start OpMode");
+        telemetry.update();
+        waitForStart();
+
+
+        if (opModeIsActive()) {
+            sleep(5000);
+            move.strafe(2600, 25);
+            //move.strafeLeft(2000,-25);
+        }
+    }
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BlueFarDelay.java
@@ -0,0 +1,38 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.util.ElapsedTime;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.CRServo;
+import org.firstinspires.ftc.vision.tfod.TfodProcessor;
+import org.firstinspires.ftc.vision.VisionPortal;
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.firstinspires.ftc.robotcore.external.tfod.Recognition;
+import com.qualcomm.robotcore.util.ElapsedTime;
+import org.firstinspires.ftc.teamcode.autonomous.Movement;
+
+@Autonomous(name = "blue-far-delay")
+public class BlueFarDelay extends LinearOpMode{
+    public void runOpMode() {
+        Movement move = new Movement();
+        move.initialize(hardwareMap);
+
+
+        // Wait for the DS start button to be touched.
+        telemetry.addData("DS preview on/off", "3 dots, Camera Stream");
+        telemetry.addData(">", "Touch Play to start OpMode");
+        telemetry.update();
+        waitForStart();
+
+
+        if (opModeIsActive()) {
+            sleep(5000);
+            move.moveForwardToThree(-1900, 100);
+            move.resetPower();
+            move.strafe(3550, 25);
+        }
+    }
+}
